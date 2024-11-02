3ea9e12cbde9a0c488da596fa3e8709ec97b5a29
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-11-02 22:21:31 +0600

    fractional knapsack problem solved

---
+++ Geedy/fractionalKnapsack.py
@@ -0,0 +1,23 @@
+class Item:
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+        self.ratio = value/weight
+    
+def fractionalKnapsack(capacity, items):
+    items.sort(key=lambda x: x.ratio, reverse=True)
+
+    totalValue = 0
+    for item in items:
+        if(capacity>=item.weight):
+            totalValue+=item.value
+            capacity-=item.weight
+        else:
+            totalValue+=item.ratio * capacity
+            break
+    return totalValue
+
+items = [Item(60, 10), Item(100, 20), Item(120, 30)]
+capacity = 50
+maxValue = fractionalKnapsack(capacity, items)
+print("maximum value: ", maxValue)
